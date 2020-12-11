1dd2e78305d2499f2666c5c2ad53ca3a67b94463
Labib <labibc01@gmail.com>
2020-12-10 21:40:39 -0500

    clean data

---
+++ model/clean_data.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+
+# data from https://www.kaggle.com/datasnaek/chess
+raw_data <- read_csv("data/games.csv")
+
+clean_data <- raw_data %>% 
+  select(-c(id, 
+            white_id, 
+            black_id,
+            created_at,
+            last_move_at, 
+            opening_ply)) %>% 
+  mutate(white_won = ifelse(winner=="white", 1 ,0))
+
+write.csv(clean_data, "data/clean_games.csv")
